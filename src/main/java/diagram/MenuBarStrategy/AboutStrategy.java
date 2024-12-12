1d063140f9fd7ba0c0d2dd80baebdcf92c44f9bc
hotsno <71658949+hotsno@users.noreply.github.com>
2024-12-11 17:28:43 -0800

    Add color code

--- src/main/java/diagram/MenuBarStrategy/AboutStrategy.java
+++ src/main/java/diagram/MenuBarStrategy/AboutStrategy.java
@@ -1,18 +1,30 @@
 package diagram.MenuBarStrategy;
 
+import diagram.DiagramElements.Box;
 import diagram.Repository;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class AboutStrategy implements MenuBarStrategy {
   @Override
   public void executeMenuAction(String itemName) {
-    JOptionPane.showMessageDialog(Repository.getInstance().getFrame(),
-            "<html><body><p>"
+    String body = "<html><body><p>"
                     + "<b>About</b><br/><br/>"
-                    + "CSC 305 Final Project<br/>"
-                    + "Made by Michael Wilson and Andrew Kulakovsky<br/>"
-                    + "</p></body></html>",
-            "About", JOptionPane.INFORMATION_MESSAGE);
+                    + "Box Connection color code:<br/>";
+
+    for (String type : Box.CONNECTION_TYPE_TO_COLOR.keySet()) {
+      Color color = Box.CONNECTION_TYPE_TO_COLOR.get(type);
+      body += "<span style=\"color: rgb("
+              + color.getRed() + "," + color.getGreen() + "," + color.getBlue() + ");\">" + type + "</span><br/>";
+    }
+
+    body += "<br/>CSC 305 Final Project<br/>"
+            + "Made by Michael Wilson and Andrew Kulakovsky<br/>"
+            + "</p></body></html>";
+
+    System.out.println(body);
+
+    JOptionPane.showMessageDialog(Repository.getInstance().getFrame(), body, "About", JOptionPane.INFORMATION_MESSAGE);
   }
 }
